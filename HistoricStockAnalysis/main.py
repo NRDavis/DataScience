a217def10cd523ac5d93713c346b1a68a460a502
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-15 22:27:54 -0400

    Add files via upload

--- HistoricStockAnalysis/main.py
+++ HistoricStockAnalysis/main.py
@@ -6,17 +6,12 @@ import csv          # used to read data from .txt/.csv files
 
 '''
 Within this project, we're going to perform historical analysis on the stock data accumulated between about 2005
-and 2017.
+and 2017. Data obtained from Kaggle data set.
 
 The user will type in the symbolic name of a stock, the application will find the pertinent data and then return a 
 visualization of the dataset.
 '''
 
-Stocks = "Stocks"
-ETFs = "ETFs"
-Data = "Data"
-cwd = os.getcwd()       # we save the current working directory (cwd) to a string
-                        # from the cwd, we can obtain open the Stocks or ETFs folders
 
 # each of the stock documents within our folder is given by it's symbolic name as follows
 #       "<symbol-name>.us.txt"
