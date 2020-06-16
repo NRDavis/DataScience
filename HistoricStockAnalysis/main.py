6be661c81eb679e9e4ee5661fe0ff63e9b2afa0a
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-15 23:58:02 -0400

    Add files via upload
    we've handled the issue of false enum strings within the plotvar() function

--- HistoricStockAnalysis/main.py
+++ HistoricStockAnalysis/main.py
@@ -54,15 +54,17 @@ def plotVar(numArr, string):
     varDict = {"date": 0, "open": 1, "high": 2, "low": 3, "close": 4, "volume": 5, "openint": 6}
     revDict = {0: "Date", 1: "Open", 2: "High", 3: "Low", 4: "Close", 5: "Volume", 6: "OpenInt"}
 
+    if not string.lower() in varDict:
+        print("Variable not defined - cannot search data")
+        return          # if we don't receive an acceptable input string, we print a statement are return
+
     itemList = []   # list
     rangeList = []
 
     for i in range(0, len(numArr)):
-        #print(numArr[i][varDict.get(string.lower())])
         itemList.append(float(numArr[i][varDict.get(string.lower())]))
         rangeList.append(i)
 
-    print(itemList)
     plt.plot(rangeList, itemList)
     plt.ylabel(revDict.get(varDict.get(string)))
     plt.xticks([min(rangeList), max(rangeList)])
@@ -78,5 +80,5 @@ fileName = searchStocks(sym)
 b = readData(fileName)
 
 # when given a symbol by the user, we want to keep the processed data around
-plotVar(b, "open")
+plotVar(b, "Open")
 
