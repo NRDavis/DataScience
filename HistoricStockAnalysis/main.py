10bded25f5a84bac48c0fcaf76651ef4c02f13c9
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-15 21:23:26 -0400

    Add files via upload
    adding search functionality for stocks and etf files

--- HistoricStockAnalysis/main.py
+++ HistoricStockAnalysis/main.py
@@ -18,20 +18,37 @@ Data = "Data"
 cwd = os.getcwd()       # we save the current working directory (cwd) to a string
                         # from the cwd, we can obtain open the Stocks or ETFs folders
 
-print(os.path.join(cwd + os.sep, Stocks))
+#print(os.path.join(cwd + os.sep, Stocks))
                         # This demonstrates how we can make filepaths using os.path methods
 
 # each of the stock documents within our folder is given by it's symbolic name as follows
 #       "<symbol-name>.us.txt"
 
+    # we search for a symbol name within our Stocks Folder, if it exists, we return path, else return none
+def searchStocks(symbol):
+    symPath = os.path.join(cwd + os.sep, Stocks, symbol+".us.txt")        #os.sep determines the best
+    if not os.path.isfile(symPath):
+        print("File does not exist")
+        return None
+    else:
+        return symPath      # returns proper path to respective stock
 
-def searchStock(symbol):
-    symPath = os.path.join(cwd + os.sep, Stocks, symbol)        #os.sep determines the best
-    return symPath
+    # if data for the symbol exists - return path. Else, print statement and return None
+def searchETFs(symbol):
+    symPath = os.path.join(cwd + os.sep, ETFs, symbol+".us.txt")
+    if not os.path.isfile(symPath):
+        print("File does not exist")
+        return None
+    else:
+        return symPath
+
 
 
 #print("This is the current working directory:"+cwd)
 print("Enter a stock symbol: ")
 sym = input()
-print(searchStock(sym))
+print(searchStocks(sym))
 
+print("\n\nEnter an ETF symbol:")
+etf = input()
+print(searchETFs(etf))
