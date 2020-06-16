b66b115deeea7f10012704ff109d12618620c685
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-16 12:50:40 -0400

    Add files via upload
    separating the functionality of our data manipulation

--- HistoricStockAnalysis/main.py
+++ HistoricStockAnalysis/main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt   # used for making visuals
 import os           # used for accessing files and data
 import csv          # used to read data from .txt/.csv files
 
+import obtain as ob             # we import all of the functions we use to search for data and read it
+import plotting as pt           # functions used to display data and save it
 
 '''
 Within this project, we're going to perform historical analysis on the stock data accumulated between about 2005
@@ -13,98 +15,7 @@ visualization of the dataset.
 
 each of the stock documents within our folder is given by it's symbolic name as follows
        "<symbol-name>.us.txt"
-
 '''
-
-
-    # we search for a symbol name within our Stocks Folder, if it exists, we return path, else return none
-def searchStocks(symbol):
-    symPath = os.path.join(os.getcwd() + os.sep, "Stocks", symbol.lower()+".us.txt")        #os.sep determines the best
-    if not os.path.isfile(symPath):
-        print("File "+symPath+" does not exist")
-        return None
-    else:
-        return symPath      # returns proper path to respective stock
-
-
-    # if data for the symbol exists - return path. Else, print statement and return None
-def searchETFs(symbol):
-    symPath = os.path.join(os.getcwd() + os.sep, "ETFs", symbol.lower() +".us.txt")
-    if not os.path.isfile(symPath):
-        print("File "+symPath+" does not exist")
-        return None
-    else:
-        return symPath
-
-
-def readData(pathName):
-    # we process our data using the with block because that'll terminate the open() with a close() when done
-    '''
-    Data follows the format of:
-        Date,Open,High,Low,Close,Volume,OpenInt
-
-        We'll use the searchStocks() or searchETFs() functions to provide our path names
-    '''
-    with open(pathName, 'r') as reader:
-        day = list(csv.reader(reader))      # we create an array of list objects
-        return np.array(day[1:])        # returns numpy array, skips first row including column names
-
-
-def plotVar(numArr, string):
-    # we define a dictionary in a manner similar to cpp enumerations
-    varDict = {"date": 0, "open": 1, "high": 2, "low": 3, "close": 4, "volume": 5, "openint": 6}
-    revDict = {0: "Date", 1: "Open", 2: "High", 3: "Low", 4: "Close", 5: "Volume", 6: "OpenInt"}
-
-    if not string.lower() in varDict:
-        print("Variable not defined - cannot search data")
-        return          # if we don't receive an acceptable input string, we print a statement are return
-    elif string.lower() == "date":
-        print("There is not point in displaying the date variable graphically")
-        return
-
-    itemList = []   # list
-    rangeList = []
-
-    for i in range(0, len(numArr)):
-        itemList.append(float(numArr[i][varDict.get(string.lower())]))
-        rangeList.append(i)
-
-    plt.plot(rangeList, itemList)
-    plt.ylabel(revDict.get(varDict.get(string)))
-    plt.xticks([min(rangeList), max(rangeList)])
-    plt.yticks([min(itemList), max(itemList)])
-    plt.show()                                      # we display the item
-    return
-
-
-def plotSave(numArr, string, symbol):
-    varDict = {"date": 0, "open": 1, "high": 2, "low": 3, "close": 4, "volume": 5, "openint": 6}
-    revDict = {0: "Date", 1: "Open", 2: "High", 3: "Low", 4: "Close", 5: "Volume", 6: "OpenInt"}
-
-    if not string.lower() in varDict:
-        print("Variable not defined - cannot search data")
-        return          # if we don't receive an acceptable input string, we print a statement are return
-    elif string.lower() == "date":
-        print("There is not point in displaying the date variable graphically")
-        return
-
-    itemList = []   # list
-    rangeList = []
-
-    for i in range(0, len(numArr)):
-        itemList.append(float(numArr[i][varDict.get(string.lower())]))
-        rangeList.append(i)
-
-    plt.plot(rangeList, itemList)
-    plt.ylabel(revDict.get(varDict.get(string)))
-    plt.xticks([min(rangeList), max(rangeList)])
-    plt.yticks([min(itemList), max(itemList)])
-    save = str(symbol.upper()) +"-"+str(string)+ ".png"
-    plt.savefig(save)
-    return
-
-
-
 
 
 
@@ -114,8 +25,8 @@ inp = "going"
 while inp != "q":
     print("Enter a stock symbol:")
     inp = input()
-    fileName = searchStocks(inp)
-    b = readData(fileName)
+    fileName = ob.searchStocks(inp)
+    b = ob.readData(fileName)
     # when given a symbol by the user, we want to keep the processed data around
-    plotVar(b, "Open")
+    pt.plotVar(b, "Open")
     #plotSave(b, "Volume", inp)
