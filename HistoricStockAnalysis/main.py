c0edbeec92146f7db2f5e8f75f43e95195b52a87
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-16 12:09:18 -0400

    Add files via upload
    cleaning main file

--- HistoricStockAnalysis/main.py
+++ HistoricStockAnalysis/main.py
@@ -10,11 +10,12 @@ and 2017. Data obtained from Kaggle data set.
 
 The user will type in the symbolic name of a stock, the application will find the pertinent data and then return a 
 visualization of the dataset.
+
+each of the stock documents within our folder is given by it's symbolic name as follows
+       "<symbol-name>.us.txt"
+
 '''
 
-
-# each of the stock documents within our folder is given by it's symbolic name as follows
-#       "<symbol-name>.us.txt"
 
     # we search for a symbol name within our Stocks Folder, if it exists, we return path, else return none
 def searchStocks(symbol):
@@ -25,6 +26,7 @@ def searchStocks(symbol):
     else:
         return symPath      # returns proper path to respective stock
 
+
     # if data for the symbol exists - return path. Else, print statement and return None
 def searchETFs(symbol):
     symPath = os.path.join(os.getcwd() + os.sep, "ETFs", symbol.lower() +".us.txt")
@@ -34,23 +36,22 @@ def searchETFs(symbol):
     else:
         return symPath
 
-'''
-Data follows the format of:
-    Date,Open,High,Low,Close,Volume,OpenInt
-    
-    We'll use the searchStocks() or searchETFs() functions to provide our path names
-'''
+
 def readData(pathName):
     # we process our data using the with block because that'll terminate the open() with a close() when done
+    '''
+    Data follows the format of:
+        Date,Open,High,Low,Close,Volume,OpenInt
+
+        We'll use the searchStocks() or searchETFs() functions to provide our path names
+    '''
     with open(pathName, 'r') as reader:
         day = list(csv.reader(reader))      # we create an array of list objects
         return np.array(day[1:])        # returns numpy array, skips first row including column names
 
 
-# we define a dictionary in a manner similar to cpp enumerations
-
-
 def plotVar(numArr, string):
+    # we define a dictionary in a manner similar to cpp enumerations
     varDict = {"date": 0, "open": 1, "high": 2, "low": 3, "close": 4, "volume": 5, "openint": 6}
     revDict = {0: "Date", 1: "Open", 2: "High", 3: "Low", 4: "Close", 5: "Volume", 6: "OpenInt"}
 
@@ -73,8 +74,8 @@ def plotVar(numArr, string):
     plt.xticks([min(rangeList), max(rangeList)])
     plt.yticks([min(itemList), max(itemList)])
     plt.show()                                      # we display the item
-    #plt.savefig(+".png")
     return
+
 
 def plotSave(numArr, string, symbol):
     varDict = {"date": 0, "open": 1, "high": 2, "low": 3, "close": 4, "volume": 5, "openint": 6}
@@ -103,6 +104,11 @@ def plotSave(numArr, string, symbol):
     return
 
 
+
+
+
+
+
 inp = "going"
 
 while inp != "q":
@@ -111,5 +117,5 @@ while inp != "q":
     fileName = searchStocks(inp)
     b = readData(fileName)
     # when given a symbol by the user, we want to keep the processed data around
-    #plotVar(b, "Open")
-    plotSave(b, "Volume", inp)
+    plotVar(b, "Open")
+    #plotSave(b, "Volume", inp)
